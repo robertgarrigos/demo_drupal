5b869ab92670f2150403d10019b3b829bbd67d0e
gkapoor1 <gaurav.k@opensenselabs.com>
2017-11-18 00:40:06 +0530

    Bug Fixes in Demo Admin Settings Form

--- src/Form/DemoAdminSettings.php
+++ src/Form/DemoAdminSettings.php
@@ -15,6 +15,27 @@ class DemoAdminSettings extends ConfigFormBase {
     return 'demo_admin_settings';
   }
 
+  public function buildForm(array $form, \Drupal\Core\Form\FormStateInterface $form_state) {
+
+    $form['demo_dump_path'] = [
+      '#type' => 'textfield',
+      '#title' => t('Snapshot file system path'),
+      '#field_prefix' => 'private://',
+      '#default_value' => variable_get('demo_dump_path', 'demo'),
+      '#required' => TRUE,
+    ];
+    
+    return parent::buildForm($form, $form_state);
+  }
+
+  public function validateForm(array &$form, \Drupal\Core\Form\FormStateInterface $form_state) {
+    if (!file_prepare_directory($form_state->getValue(['demo_dump_path']), FILE_CREATE_DIRECTORY)) {
+      $form_state->setErrorByName('demo_dump_path', t('The snapshot directory %directory could not be created.', [
+        '%directory' => $form_state->getValue(['demo_dump_path'])
+        ]));
+    }
+  }
+
   /**
    * {@inheritdoc}
    */
@@ -26,10 +47,6 @@ class DemoAdminSettings extends ConfigFormBase {
     }
     $config->save();
 
-    if (method_exists($this, '_submitForm')) {
-      $this->_submitForm($form, $form_state);
-    }
-
     parent::submitForm($form, $form_state);
   }
 
@@ -38,35 +55,6 @@ class DemoAdminSettings extends ConfigFormBase {
    */
   protected function getEditableConfigNames() {
     return ['demo.settings'];
-  }
-
-  public function buildForm(array $form, \Drupal\Core\Form\FormStateInterface $form_state) {
-    if (!file_stream_wrapper_valid_scheme('private')) {
-      $form_state->setErrorByName('', t('The <a href="@file-settings-url">private filesystem</a> must be configured in order to create or load snapshots.', [
-        '@file-settings-url' => url('admin/config/media/file-system', [
-          'query' => drupal_get_destination()
-          ])
-        ]));
-    }
-
-    $form['demo_dump_path'] = [
-      '#type' => 'textfield',
-      '#title' => t('Snapshot file system path'),
-      '#field_prefix' => 'private://',
-      '#default_value' => variable_get('demo_dump_path', 'demo'),
-      '#required' => TRUE,
-    ];
-    $form['#validate'][] = 'demo_admin_settings_validate';
-
-    return parent::buildForm($form, $form_state);
-  }
-
-  public function validateForm(array &$form, \Drupal\Core\Form\FormStateInterface $form_state) {
-    if (!file_prepare_directory($form_state->getValue(['demo_dump_path']), FILE_CREATE_DIRECTORY)) {
-      $form_state->setErrorByName('demo_dump_path', t('The snapshot directory %directory could not be created.', [
-        '%directory' => $form_state->getValue(['demo_dump_path'])
-        ]));
-    }
   }
 
 }
